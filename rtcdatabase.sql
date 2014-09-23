9fa5691ee55e15b0e5188e14f74fced5d79975cc
Krishnan <krishnandon@gmail.com>
2014-09-22 23:27:13 -0700

    rtcdatabase.sql modified version

--- rtcdatabase.sql
+++ rtcdatabase.sql
@@ -2,13 +2,19 @@
 -- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: Sep 23, 2014 at 03:50 AM
--- Server version: 5.5.34
--- PHP Version: 5.5.10
+-- Host: 127.0.0.1
+-- Generation Time: Sep 23, 2014 at 08:25 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `rtcdatabase`
@@ -20,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(20) DEFAULT NULL,
   `first_name` varchar(20) DEFAULT NULL,
@@ -29,7 +35,7 @@ CREATE TABLE `user` (
   `age` int(3) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `user`
@@ -37,7 +43,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `user_name`, `first_name`, `last_name`, `email_id`, `age`, `password`) VALUES
 (1, 'shivaseshan', 'Shiva', 'Ramaseshan', 'shiva@mail.com', 26, '69f404925df883e0e5579d65b7768e7c'),
-(2, 'shiva', 'shiva', 'rama', 'shiva@mail.com', 22, '69f404925df883e0e5579d65b7768e7c');
+(2, 'shiva', 'shiva', 'rama', 'shiva@mail.com', 22, '69f404925df883e0e5579d65b7768e7c'),
+(3, 'krishnan', 'Krishnan', 'Narayanan', 'kknaraya@asu.edu', 24, 'fea209e251aade9628951d59f6108caa');
 
 -- --------------------------------------------------------
 
@@ -45,19 +52,37 @@ INSERT INTO `user` (`user_id`, `user_name`, `first_name`, `last_name`, `email_id
 -- Table structure for table `video`
 --
 
-CREATE TABLE `video` (
+CREATE TABLE IF NOT EXISTS `video` (
   `name` varchar(256) NOT NULL,
   `source` varchar(256) NOT NULL,
   `category` varchar(256) NOT NULL,
   `duration` int(11) NOT NULL,
   `start` timestamp NULL DEFAULT NULL,
-  `end` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `end` timestamp NULL DEFAULT NULL,
+  `video_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`video_id`),
+  KEY `fk` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `video`
 --
 
-INSERT INTO `video` (`name`, `source`, `category`, `duration`, `start`, `end`) VALUES
-('kristi', 'kristi.mov', 'general', 30, NULL, NULL),
-('Bon jovi', 'bonjovi.mp4', 'music', 60, NULL, NULL);
+INSERT INTO `video` (`name`, `source`, `category`, `duration`, `start`, `end`, `video_id`, `user_id`) VALUES
+('kristi', 'kristi.mov', 'general', 30, NULL, NULL, 1, 1),
+('Bon jovi', 'bonjovi.mp4', 'music', 60, NULL, NULL, 2, 1);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `video`
+--
+ALTER TABLE `video`
+  ADD CONSTRAINT `fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
